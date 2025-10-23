753d8ac2f86ce59bd56f0afa9c130ec7e27a2343
Oleh Skibinskyi <45209469+leemonch1k-space@users.noreply.github.com>
2025-10-23 20:09:50 +0300

    update function and annotation
    update function for work with kwargs and change annotation to Any

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     saved_cache = {}
-    
-    def wrapper(*args, **kwargs) -> int:
-        nonlocal saved_cache
 
-        if args in saved_cache:
+    def wrapper(*args, **kwargs) -> Any:
+        cache_key = (args, tuple(sorted(kwargs.items())))
+
+        if cache_key in saved_cache:
             print("Getting from cache")
-            return saved_cache[args]
-        else:
-            print("Calculating new result")
-            saved_cache[args] = func(*args, **kwargs)
-            return saved_cache[args]
+            return saved_cache[cache_key]
+
+        cache_value = func(*args, **kwargs)
+        saved_cache[cache_key] = cache_value
+        print("Calculating new result")
+        return cache_value
 
     return wrapper
